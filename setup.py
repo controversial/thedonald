7374e0bb0bbb3afc70e736707c8f651fd2ac7fa8
The-Penultimate-Defenestrator <ldt@deentaylor.com>
2016-07-29 15:59:20 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="thedonald",
+    version="1.0.0",
+    description=("A Markov text generator generating fake Trump quotes from"
+                 " Trump's tweets."),
+    author="Luke Taylor",
+    author_email="luke@deentaylor.com",
+    url="http://luke.deentaylor.com/",
+    install_requires=[
+        "tweepy",
+    ],
+)
